eebf49215c19b3dcad7ca68f25e2ea05141c1e64
Hazel Wu <tinazx056@gmail.com>
2022-09-14 01:06:38 +0800

    feat: set vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+import { createApp } from 'vue'
+import App from './vue/app.vue'
+
+createApp({
+    components: {
+        App
+    },
+}).mount('#app')
---
+++ resources/js/vue/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Hello vue121231233</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+</style>
